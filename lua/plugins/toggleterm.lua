9d6547fce4eee00c669f63b8c226315ee073bd06
gawakawa <hirakuhasegawa@gmail.com>
2025-01-02 00:40:15 +0900

    :technologist: Set keymap for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,6 +6,12 @@ return {
       open_mapping = [[<c-t>]],
       start_in_insert = true,
     }
+
+    vim.keymap.set("n", "<c-1>", ":1ToggleTerm<CR>")
+    vim.keymap.set("n", "<c-2>", ":2ToggleTerm<CR>")
+    vim.keymap.set("n", "<c-3>", ":3ToggleTerm<CR>")
+    vim.keymap.set("n", "<c-4>", ":4ToggleTerm<CR>")
+    vim.keymap.set("n", "<c-5>", ":5ToggleTerm<CR>")
   end,
 }
 
