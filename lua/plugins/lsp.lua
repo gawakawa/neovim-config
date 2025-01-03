292588e4fc8e7dfc58a347fe34a85d7ddbabfa3b
gawakawa <hirakuhasegawa@gmail.com>
2025-01-04 03:17:56 +0900

    :sparkles: Introduce some lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,8 +7,12 @@ return {
     require("mason").setup()
     require("mason-lspconfig").setup()
     require("lspconfig").hls.setup {}
+    require("lspconfig").idris2_lsp.setup {}
     require("lspconfig").lua_ls.setup {}
+    require("lspconfig").prismals.setup {}
+    require("lspconfig").purescriptls.setup {}
     require("lspconfig").rust_analyzer.setup {}
+    require("lspconfig").ts_ls.setup {}
   end,
 }
 
