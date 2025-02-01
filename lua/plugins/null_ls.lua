bc4f8c2b89774d13dc7a37e0809fb63ade44eb33
gawakawa <hirakuhasegawa@gmail.com>
2025-02-01 17:05:43 +0900

    🐛 Enable to use PureScript LSP

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -5,10 +5,18 @@ return {
   },
   config = function()
     local null_ls = require("null-ls")
+
+    vim.filetype.add({
+      extension = {
+        purs = "purescript"
+      }
+    })
+
     null_ls.setup {
       sources = {
         null_ls.builtins.code_actions.eslint,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.purs_tidy,
       },
     }
   end,
